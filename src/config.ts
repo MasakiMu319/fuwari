e8b2ec2e8819d6ffe64db62c2f28529372bd2d45
MasakiMu319 <bancangbaize@gmail.com>
2024-07-31 11:05:19 +0800

    💚

--- src/config.ts
+++ src/config.ts
@@ -47,7 +47,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'MasakiMu319',
   bio: 'Petit à petit, l’oiseau fait son nid ; chaque effort compte.',
   links: [
